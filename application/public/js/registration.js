00c34be960d5ecb3a68f4c931daba6d1018dbf21
emhuang3 <emilyhuangx2012@gmail.com>
2021-11-05 06:19:12 -0700

    added all files to new project

---
+++ application/public/js/registration.js
@@ -0,0 +1,57 @@
+
+const form = document.getElementById("registration-form");
+
+form.addEventListener("submit", (e)=>{
+    
+    usernameReq();
+    passwordReq();
+
+    if(usernameReq() == false || passwordReq() == false){
+        e.preventDefault();
+       
+    }
+   
+    
+});
+
+
+
+function usernameReq(){
+    var username = document.getElementById("username-content").value;
+    if(/^([a-z])[a-z0-9]{2,}/gi.test(username)){
+        document.getElementById("username-input-failure").style.visibility = "hidden";
+    }
+    else{
+        document.getElementById("username-input-failure").style.visibility = "visible";
+        return false;
+    }
+    return true;
+
+}
+
+function passwordReq(){
+    var password1 = document.getElementById("password").value;
+    var password2 = document.getElementById("password-confirmation").value;
+
+    if(password1 != password2){
+        
+        document.getElementById("password-input-failure").style.visibility = "visible";
+        return false;
+        
+    }
+    if(/^(?=.*[A-Z])(?=.*[0-9])(?=.*[(\-+!@#\$\^&\*])(?=.{8,})/.test(password1)){
+        document.getElementById("password-input-failure").style.visibility = "hidden";
+        return true;
+    }
+    else{
+        document.getElementById("password-input-failure").style.visibility = "visible";
+        return false;
+       
+    }
+}
+
+
+
+
+
+
--- application/routes/index.js
+++ application/routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:"Emily Huang" });
 });
 
+router.get('/login', (req, res, next)=>{
+  res.render('login');
+})
+
+router.get('/registration', (req, res, next)=>{
+  res.render('registration');
+})
+
+router.get('/home', (req, res, next)=>{
+  res.render('home');
+})
+
+router.get('/postimage', (req, res, next)=>{
+  res.render('postimage');
+})
+
+router.get('/viewpost', (req, res, next)=>{
+  res.render('viewpost');
+})
+
 module.exports = router;
