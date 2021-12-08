1a40a5334e705448c3b33f34708aa741bce37af3
emhuang3 <emilyhuangx2012@gmail.com>
2021-12-08 06:10:26 -0800

    view post selected image and post info implemented

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var isLoggedIn = require('../middleware/routeprotecters').userIsLoggedIn;
 var getRecentPosts = require('../middleware/postsmiddleware').getRecentPosts;
+var db = require("../conf/database")
 
 /* GET home page. */
 router.get('/', getRecentPosts, function(req, res, next) {
@@ -28,6 +29,28 @@ router.get('/viewpost', (req, res, next)=>{
 router.use('/postimage', isLoggedIn);
 router.get('/postimage', (req, res, next)=>{
   res.render('postimage');
+})
+
+router.get('/post/:id(\\d+)', (req, res, next)=>{
+  let baseSQL = "\
+  SELECT u.username, p.title, p.description, p.photopath, p.createdAt\
+  FROM users u\
+  JOIN posts p\
+  ON u.id=fk_userId\
+  WHERE p.id=?;";
+
+  let postId = req.params.id;
+  db.execute(baseSQL, [postId])
+  .then(([results, fields])=>{
+    if(results && results.length){
+       let post = results[0];
+       res.render('viewpost', {currentPost: post});
+    }
+    else{
+      req.flash('error', 'This is not the post you are looking for!');
+      res.redirect('/')
+    }
+  })
 })
 
 module.exports = router;
