19cae0a23b1705b4bd3b009f017d00a1212cbd85
emhuang3 <emilyhuangx2012@gmail.com>
2021-12-11 07:09:18 -0800

    more work on comments backend

--- application/routes/comments.js
+++ application/routes/comments.js
@@ -4,8 +4,63 @@ const {successPrint, errorPrint} = require("../helpers/debug/debugprinters");
 const { create } = require("../models/comments");
 
 router.post('/create', (req, res, next)=>{
-    console.log(req.body);
-    res.json("we got your comment");
+    if(!req.session.username){
+        errorPrint("You must be logged in to comment.");
+        res.json({
+            code: -1,
+            status: "danger",
+            message: "You must be logged in to comment." 
+        });
+    }else{
+        let {comment, postId } = req.body;
+        let username = req.session.username;
+        let userId = req.session.userId;
+
+        create(userId, postId, comment)
+        .then((wasSuccessful)=>{
+            if(wasSuccessful != -1){
+                successPrint(`Comment was created for ${username}`);
+                res.json({
+                    code: 1,
+                    status: "success",
+                    message: "Comment created.",
+                    comment: comment,
+                    username: username
+                })
+            }else{
+                errorPrint('Comment was not saved.')
+                res.json({
+                    code: -1,
+                    status: "danger",
+                    message: "Comment was not created."
+                })
+            }
+        }).catch((err) =>next(err));
+        }
+    // let {comment, postId } = req.body;
+    // let username = 'jack'; //req.session.username
+    // let userId = 10;    //req.session.userId
+
+    // create(userId, postId, comment)
+    // .then((wasSuccessful)=>{
+    //     if(wasSuccessful != -1){
+    //         successPrint(`Comment was created for ${username}`);
+    //         res.json({
+    //             code: 1,
+    //             status: "success",
+    //             message: "Comment created.",
+    //             comment: comment,
+    //             username: username
+    //         })
+    //     }else{
+    //         errorPrint('Comment was not saved.')
+    //         res.json({
+    //             code: -1,
+    //             status: "danger",
+    //             message: "Comment was not created."
+    //         })
+    //     }
+    // }).catch((err) =>next(err));
 })
 
 module.exports = router;
